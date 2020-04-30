c7d6f11d011a876963a7dd2db5fac7fdde7e8636
d_liyanage <admliyanage@gmail.com>
2020-04-30 14:21:16 +0530

    shower.png added

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,116 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <full-page :options="options" id="fullpage">
+      <div class="section">
+        <div class="container">
+          <img src="./assets/shower.png" alt="prevening icon" />
+
+          <div class="quote-symbol">❝</div>
+          <p class="quote">
+            <a href="#">Quote Goes Here</a>
+          </p>
+          <div class="quote-symbol">❞</div>
+          <div class="details">
+            <div class="details-author">Author</div>
+            <div class="stats">1234 upvotes</div>
+          </div>
+        </div>
+      </div>
+      <!-- ❝❞ -->
+
+      <div class="section">
+        <h3>Section 2</h3>
+      </div>
+
+      <div class="section">
+        <h3>Section 3</h3>
+      </div>
+    </full-page>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  // components: {
+  //   HelloWorld
+  // }
+  data() {
+    return {
+      options: {
+        licenseKey: "YOUR_KEY_HERE",
+        afterLoad: this.afterLoad,
+        scrollOverflow: true,
+        scrollBar: false,
+        menu: "#menu",
+        navigation: true,
+        anchors: ["page1", "page2", "page3"],
+        sectionsColor: [
+          "#41b883",
+          "#ff5f45",
+          "#0798ec",
+          "#fec401",
+          "#1bcee6",
+          "#ee1a59",
+          "#2c3e4f",
+          "#ba5be9",
+          "#b4b8ab"
+        ]
+      }
+    };
   }
-}
+};
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Homemade+Apple&display=swap");
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: sans-serif;
+  color: #2d3748;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+}
+
+h3 {
+  margin: 0;
+}
+
+.container {
+  margin: auto;
+  max-width: 800px;
+  padding: 80px 16px;
+}
+
+.section {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+.quote-symbol {
+  font-size: 64px;
+  line-height: 0;
+  margin-top: 50px;
+  color: white;
+}
+
+.quote a {
+  text-decoration: none;
+  font-family: "Homemade Apple", cursive;
+  color: white;
+  font-size: 36px;
+  line-height: 1.5;
+}
+
+.quote a:hover {
+  color: #edf2f7;
+}
+
+.details-stats {
+  margin-top: 4px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import VueFullPage from 'vue-fullpage.js';
 
-Vue.config.productionTip = false
+Vue.use(VueFullPage);
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
